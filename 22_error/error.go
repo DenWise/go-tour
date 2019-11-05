635ced5a0ec94d0bb4661f67ae33a3c9342994c6
DenWise <denny.wise.cropp@gmail.com>
2019-11-06 00:37:48 +0300

    go

---
+++ 22_error/error.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type MyError struct {
+    When time.Time
+    What string
+}
+
+func (e *MyError) Error() string {
+    return fmt.Sprintf("at %v, %s\n", e.When, e.What) // returns a string describing error
+}
+
+func run() error { // returns a pointer to value of type "error"
+    return &MyError{
+        time.Now(),
+        "it didn't work",
+    }
+}
+
+func main() {
+    if err := run(); err != nil {
+        fmt.Println(err)
+    }
+}
